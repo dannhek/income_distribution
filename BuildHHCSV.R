499a6cbfa9f05545f4e315b0355af1f572e7ecb3
Dann <Dannhek@gmail.com>
2016-05-20 23:11:29 -0500

    Add Individual Comparison Scripts first pass

--- BuildHHCSV.R
+++ BuildHHCSV.R
@@ -17,8 +17,8 @@ library(sas7bdat.parso) 	# load the sas7bdat.parso (imports binary/compressed sa
 library(quantmod)
 
 setwd("~/R Income Distribution/CPS Data")
-cps.years.to.download <- c(2015,2010,2005,2000)
-source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Current%20Population%20Survey/download%20all%20microdata.R" , prompt = FALSE , echo = TRUE )
+# cps.years.to.download <- c(2015,2010,2005,2000)
+# source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Current%20Population%20Survey/download%20all%20microdata.R" , prompt = FALSE , echo = TRUE )
 
 options(survey.replicates.mse = TRUE)
 dbfolder <- paste0( getwd() , "/MonetDB" )
@@ -56,10 +56,13 @@ cf <- avg.cpi/as.numeric(avg.cpi['2015']) #using 2015 as the base year
 
 dfFull <- rbind(df2015,df2010,df2005,df2000)
 dfFull$year <- as.character(dfFull$year)
-dfFull$cpi_adj      <- cf[dfFull$year]
+cf <- as.data.frame(cf)
+rownames(cf) <- year(rownames(cf))
+names(cf) <- "cpi_adj"
+dfFull <- merge(dfFull,cf,by.x="year",by.y="row.names")
 dfFull$adj_h_income <- dfFull$h_income * dfFull$cpi_adj
 
 ##Write to File
 write.csv(dfFull, "hhwagessalary.csv")
 
-
+dbDisconnect(db)
